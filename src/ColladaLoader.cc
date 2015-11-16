499d381822099a1fe2c3721e8c6e070c3efe25df
Ian Chen <ichen@osrfoundation.org>
2015-11-16 09:10:26 -0800

    fix load file check

--- src/ColladaLoader.cc
+++ src/ColladaLoader.cc
@@ -117,7 +117,7 @@ Mesh *ColladaLoader::Load(const std::string &_filename)
   }
 
   this->dataPtr->filename = _filename;
-  if (!xmlDoc.LoadFile(_filename.c_str()) != tinyxml2::XML_NO_ERROR)
+  if (xmlDoc.LoadFile(_filename.c_str()) != tinyxml2::XML_NO_ERROR)
     ignerr << "Unable to load collada file[" << _filename << "]\n";
 
   this->dataPtr->colladaXml = xmlDoc.FirstChildElement("COLLADA");
