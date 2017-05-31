6524dd238bf03246c389dd48d151db0138b9bb28
Shane Loretz <sloretz@osrfoundation.org>
2017-05-31 14:09:38 -0700

    Added WorkerPool
    WorkerPool manages worker threads. They pool is given work, and threads process it in parallel.
    Work is given as function calls.

---
+++ include/ignition/common/WorkerPool.hh
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef IGNITION_COMMON_WORKER_POOL_HH_
+#define IGNITION_COMMON_WORKER_POOL_HH_
+
+#include <functional>
+#include <memory>
+
+#include <ignition/common/Time.hh>
+
+namespace ignition
+{
+  namespace common
+  {
+    /// \brief forward declaration
+    class WorkerPoolPrivate;
+
+    /// \brief A pool of worker threads that do stuff in parallel
+    class WorkerPool
+    {
+      /// \brief creates worker threads
+      public: WorkerPool();
+
+      /// \brief closes worker threads
+      public: ~WorkerPool();
+
+      /// \brief Adds work to the worker pool with optional callback
+      /// \param[in] _work function to do one piece of work
+      /// \param[in] _cb optional callback when the work is done
+      /// \remark _work should be bound with arguments so it does the work and
+      //               returns within a finite amount of time
+      public: void AddWork(std::function<void()> _work,
+                  std::function<void()> _cb = std::function<void()>());
+
+      /// \brief Waits until all work is done and threads are idle
+      /// \param[in] _timeout How long to wait, default to forever
+      /// \returns true if all work was finished
+      /// \remarks The return value can be false even when waiting forever if
+      //           the WorkerPool is destructed before all work is completed
+      public: bool WaitForResults(const Time &_timeout = Time::Zero);
+
+      /// \brief private implementation pointer
+      private: std::unique_ptr<WorkerPoolPrivate> dataPtr;
+    };
+  }
+}
+
+#endif
