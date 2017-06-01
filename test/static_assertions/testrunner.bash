b0cf5da3f08239d664d586183d0f81f041e3fa59
Shane Loretz <sloretz@osrfoundation.org>
2017-06-01 10:31:46 -0700

    OSX compatibility

--- test/static_assertions/testrunner.bash
+++ test/static_assertions/testrunner.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # mess with output to confuse jenkins build failure indications because these
-# tests pass when compilation fails
-eval $@ 2>&1 | sed s/error\\\|failed\\\|Error/[Censored\ by\ testrunner.bash]/g
+# tests pass when compilation fails. Multiple pipes for OSX compatibility
+eval $@ 2>&1 | sed s/error/[Censored\ by\ testrunner.bash]/g | sed s/Error/[Censored\ by\ testrunner.bash]/g | sed s/Fail/[Censored\ by\ testrunner.bash]/g
