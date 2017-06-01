843f33446184ce5c54ad066740d3fac64b6e976a
Shane Loretz <sloretz@osrfoundation.org>
2017-06-01 10:15:30 -0700

    Remove case insensitive flag because it's not supported on OSX

--- test/static_assertions/testrunner.bash
+++ test/static_assertions/testrunner.bash
@@ -2,4 +2,4 @@
 
 # mess with output to confuse jenkins build failure indications because these
 # tests pass when compilation fails
-eval $@ 2>&1 | sed s/error\\\|failed/[Censored\ by\ testrunner.bash]/gI
+eval $@ 2>&1 | sed s/error\\\|failed\\\|Error/[Censored\ by\ testrunner.bash]/g
