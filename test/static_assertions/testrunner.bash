ff8c516ae3ea3280c4720c6eef2a9ce9284b777c
Shane Loretz <sloretz@osrfoundation.org>
2017-06-13 13:45:42 -0700

    Apply patch from scpeters

--- test/static_assertions/testrunner.bash
+++ test/static_assertions/testrunner.bash
@@ -2,4 +2,6 @@
 
 # mess with output to confuse jenkins build failure indications because these
 # tests pass when compilation fails. Multiple pipes for OSX compatibility
-eval $@ 2>&1 | sed s/error/[Censored\ by\ testrunner.bash]/g | sed s/Error/[Censored\ by\ testrunner.bash]/g | sed s/Fail/[Censored\ by\ testrunner.bash]/g
+eval $@ 2>&1 \
+  | sed s/[Ee]rror/[Censored\ by\ testrunner.bash]/g \
+  | sed s/Fail/[Censored\ by\ testrunner.bash]/g
