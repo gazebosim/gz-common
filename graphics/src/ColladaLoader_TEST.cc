35c909afc8e5cf5f3ab278dac2af4793cc42d1b6
Steve Peters <scpeters@openrobotics.org>
2020-02-13 14:51:42 -0800

    add lines to test confirming NodeAnimation name

--- graphics/src/ColladaLoader_TEST.cc
+++ graphics/src/ColladaLoader_TEST.cc
@@ -204,7 +204,9 @@ TEST_F(ColladaLoader, LoadBoxWithAnimationOutsideSkeleton)
   common::SkeletonAnimation *anim = skeleton->Animation(0);
   EXPECT_EQ(1u, anim->NodeCount());
   EXPECT_TRUE(anim->HasNode("Armature"));
-  EXPECT_NE(nullptr, anim->NodeAnimationByName("Armature"));
+  auto nodeAnimation = anim->NodeAnimationByName("Armature");
+  EXPECT_NE(nullptr, nodeAnimation);
+  EXPECT_EQ("Armature", nodeAnimation->Name());
   auto poseStart = anim->PoseAt(0.04166662);
   math::Matrix4d expectedTrans = math::Matrix4d(
       1, 0, 0, 1,
@@ -285,7 +287,9 @@ TEST_F(ColladaLoader, LoadBoxNestedAnimation)
   common::SkeletonAnimation *anim = skeleton->Animation(0);
   EXPECT_EQ(1u, anim->NodeCount());
   EXPECT_TRUE(anim->HasNode("Bone"));
-  EXPECT_NE(nullptr, anim->NodeAnimationByName("Bone"));
+  auto nodeAnimation = anim->NodeAnimationByName("Bone");
+  EXPECT_NE(nullptr, nodeAnimation);
+  EXPECT_EQ("Bone", nodeAnimation->Name());
   auto poseStart = anim->PoseAt(0);
   math::Matrix4d expectedTrans = math::Matrix4d(
       1, 0, 0, 1,
