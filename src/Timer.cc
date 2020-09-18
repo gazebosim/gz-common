6805de09a7632c017387cf43b330d0c3aee2cf03
ahcorde <ahcorde@gmail.com>
2020-09-18 11:09:08 +0200

    Avoided duplicated code
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/Timer.cc
+++ src/Timer.cc
@@ -51,36 +51,18 @@ bool Timer::Running() const
   return this->running;
 }
 
+#ifndef _WIN32
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 //////////////////////////////////////////////////
 Time Timer::Elapsed() const
 {
-  if (this->running)
-  {
-    std::chrono::steady_clock::time_point currentTime;
-    currentTime = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff = currentTime - this->start;
-#ifndef _WIN32
-# pragma GCC diagnostic push
-# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-    return Time(diff.count());
-#ifndef _WIN32
-# pragma GCC diagnostic pop
-#endif
-  }
-  else
-  {
-    std::chrono::duration<double> diff = this->stop - this->start;
-#ifndef _WIN32
-# pragma GCC diagnostic push
-# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-    return Time(diff.count());
-#ifndef _WIN32
-# pragma GCC diagnostic pop
-#endif
-  }
+  return common::Time(this->ElapsedTime().count());
 }
+#ifndef _WIN32
+# pragma GCC diagnostic pop
+#endif
 
 //////////////////////////////////////////////////
 std::chrono::duration<double> Timer::ElapsedTime() const
