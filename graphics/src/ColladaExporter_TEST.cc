c2f86eb46c9b73cc1d1e14aad30e0f41932ac2cd
Gonzo <42421541+gonzodepedro@users.noreply.github.com>
2020-11-18 14:40:16 -0300

    Add transformation matrix to ColladaExport (#100)
    This adds transformation matrixes to nodes in collada files when exporting
    
    Connects to: ignitionrobotics/ign-gazebo#307
    
    Signed-off-by: Gonzalo de Pedro <gonzalo@depedro.com.ar>
    
    Co-authored-by: Michael Carroll <michael@openrobotics.org>

--- graphics/src/ColladaExporter_TEST.cc
+++ graphics/src/ColladaExporter_TEST.cc
@@ -18,10 +18,11 @@
 #include "tinyxml2.h"
 
 #include "test_config.h"
-#include "ignition/common/Mesh.hh"
-#include "ignition/common/SubMesh.hh"
 #include "ignition/common/ColladaLoader.hh"
 #include "ignition/common/ColladaExporter.hh"
+#include "ignition/common/Filesystem.hh"
+#include "ignition/common/Mesh.hh"
+#include "ignition/common/SubMesh.hh"
 #include "test/util.hh"
 
 #ifdef _WIN32
@@ -191,6 +192,123 @@ TEST_F(ColladaExporter, ExportCordlessDrill)
 
   // Remove temp directory
   common::removeAll(pathOut + "/tmp");
+}
+
+/////////////////////////////////////////////////
+TEST_F(ColladaExporter, ExportMeshWithSubmeshes)
+{
+  std::string boxFilenameIn = std::string(PROJECT_SOURCE_PATH) +
+      "/test/data/box.dae";
+
+  common::ColladaLoader loader;
+  const common::Mesh *boxMesh = loader.Load(
+      boxFilenameIn);
+
+  const common::Mesh *drillMesh = loader.Load(
+      common::joinPaths(PROJECT_SOURCE_PATH,
+      "/test/data/cordless_drill/meshes/cordless_drill.dae"));
+
+  common::Mesh outMesh;
+  std::weak_ptr<common::SubMesh> subm;
+  std::vector<math::Matrix4d> subMeshMatrix;
+  math::Pose3d localPose = math::Pose3d::Zero;
+
+  int i = outMesh.AddMaterial(
+    boxMesh->MaterialByIndex(
+      boxMesh->SubMeshByIndex(0).lock()->MaterialIndex()));
+  subm = outMesh.AddSubMesh(*boxMesh->SubMeshByIndex(0).lock().get());
+  subm.lock()->SetMaterialIndex(i);
+
+  localPose.SetX(10);
+  math::Matrix4d matrix(localPose);
+  subMeshMatrix.push_back(matrix);
+
+  i = outMesh.AddMaterial(
+    drillMesh->MaterialByIndex(
+      drillMesh->SubMeshByIndex(0).lock()->MaterialIndex()));
+  subm = outMesh.AddSubMesh(*drillMesh->SubMeshByIndex(0).lock().get());
+  subm.lock()->SetMaterialIndex(i);
+
+  localPose.SetX(-10);
+  matrix = math::Matrix4d(localPose);
+  subMeshMatrix.push_back(matrix);
+
+  std::string pathOut = common::joinPaths(common::cwd(), "tmp");
+  common::createDirectories(pathOut);
+
+  common::ColladaExporter exporter;
+  exporter.Export(&outMesh,
+      common::joinPaths(pathOut, "mesh_with_submeshes"), true, subMeshMatrix);
+
+  // Check .dae file
+  tinyxml2::XMLDocument xmlDoc;
+  std::string filename = common::joinPaths(pathOut,
+     "mesh_with_submeshes/meshes/mesh_with_submeshes.dae");
+
+  EXPECT_EQ(xmlDoc.LoadFile(filename.c_str()), tinyxml2::XML_SUCCESS);
+
+  ASSERT_TRUE(xmlDoc.FirstChildElement("COLLADA") != nullptr);
+  ASSERT_TRUE(xmlDoc.FirstChildElement(
+      "COLLADA")->FirstChildElement("library_geometries") != nullptr);
+
+  tinyxml2::XMLElement *geometryXml = xmlDoc.FirstChildElement("COLLADA")
+      ->FirstChildElement("library_geometries")
+      ->FirstChildElement("geometry");
+  ASSERT_TRUE(geometryXml != nullptr);
+
+  for (unsigned int j = 0; j < outMesh.SubMeshCount(); ++j)
+  {
+    unsigned int countMeshInt =
+      outMesh.SubMeshByIndex(j).lock()->VertexCount()*3;
+    char countMesh[100];
+    snprintf(countMesh, sizeof(countMesh), "%u", countMeshInt);
+
+    const char *countDae = geometryXml
+       ->FirstChildElement("mesh")
+       ->FirstChildElement("source")
+       ->FirstChildElement("float_array")
+       ->Attribute("count");
+
+    EXPECT_STREQ(countDae, countMesh);
+
+    geometryXml = geometryXml->NextSiblingElement("geometry");
+  }
+
+  tinyxml2::XMLElement *nodeXml = xmlDoc.FirstChildElement("COLLADA")
+      ->FirstChildElement("library_visual_scenes")
+      ->FirstChildElement("visual_scene")
+      ->FirstChildElement("node");
+  ASSERT_TRUE(nodeXml != nullptr);
+
+  for (unsigned int j = 0; j < outMesh.SubMeshCount(); ++j)
+  {
+    std::ostringstream fillData;
+    fillData.precision(8);
+    fillData << std::fixed;
+    fillData << subMeshMatrix.at(j);
+
+    std::string matrixStr = nodeXml->FirstChildElement("matrix")->GetText();
+    EXPECT_EQ(matrixStr, fillData.str());
+
+    nodeXml = nodeXml->NextSiblingElement("node");
+  }
+
+  // Reload mesh and compare
+  const common::Mesh *meshReloaded = loader.Load(common::joinPaths(pathOut,
+       "mesh_with_submeshes/meshes/mesh_with_submeshes.dae"));
+
+  EXPECT_EQ(outMesh.Name(), meshReloaded->Name());
+  EXPECT_EQ(outMesh.SubMeshCount(), meshReloaded->SubMeshCount());
+  EXPECT_EQ(outMesh.MaterialCount(),
+      meshReloaded->MaterialCount());
+  EXPECT_EQ(outMesh.IndexCount(), meshReloaded->IndexCount());
+  EXPECT_EQ(outMesh.VertexCount(), meshReloaded->VertexCount());
+  EXPECT_EQ(outMesh.NormalCount(), meshReloaded->NormalCount());
+  EXPECT_EQ(outMesh.TexCoordCount(),
+      meshReloaded->TexCoordCount());
+
+  // Remove temp directory
+  common::removeAll(pathOut);
 }
 
 /////////////////////////////////////////////////
