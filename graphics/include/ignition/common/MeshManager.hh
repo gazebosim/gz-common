b337f4e0af725cd6dc1b3942c2114eaf311a9817
Alejandro Hernández Cordero <ahcorde@gmail.com>
2021-11-09 06:39:33 +0100

    Added method to remove meshes from the MeshManager (#222)
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    Signed-off-by: Nate Koenig <nate@openrobotics.org>
    
    Co-authored-by: Louise Poubel <louise@openrobotics.org>
    Co-authored-by: Michael Carroll <michael@openrobotics.org>
    Co-authored-by: Nate Koenig <nate@openrobotics.org>
    Co-authored-by: Nate Koenig <nkoenig@users.noreply.github.com>

--- graphics/include/ignition/common/MeshManager.hh
+++ graphics/include/ignition/common/MeshManager.hh
@@ -100,6 +100,15 @@ namespace ignition
       /// \param[in] the mesh to add.
       public: void AddMesh(Mesh *_mesh);
 
+      /// \brief Remove a mesh based on a name.
+      /// \param[in] _name Name of the mesh to remove.
+      /// \return True if the mesh was removed, false if the mesh with the
+      /// provided name could not be found.
+      public: bool RemoveMesh(const std::string &_name);
+
+      /// \brief Remove all meshes.
+      public: void RemoveAll();
+
       /// \brief Get a mesh by name.
       /// \param[in] _name the name of the mesh to look for
       /// \return the mesh or nullptr if not found
