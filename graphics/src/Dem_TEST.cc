da536ed278055a1d6071a8cc8f56feb007f4aa70
Steve Peters <scpeters@openrobotics.org>
2022-01-19 11:39:57 -0800

    Revert "Geospatial component for heightmap & DEMs (#267)"
    This reverts commit 28eb6c7fb564f612a7c7d9a60b11fb9ad8da52b8.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- graphics/src/Dem_TEST.cc
+++ graphics/src/Dem_TEST.cc
@@ -16,7 +16,6 @@
 */
 
 #include <gtest/gtest.h>
-#include <limits>
 #include <ignition/math/Angle.hh>
 #include <ignition/math/Vector3.hh>
 
@@ -27,8 +26,10 @@ using namespace ignition;
 
 class DemTest : public common::testing::AutoLogFixture { };
 
+#ifdef HAVE_GDAL
+
 /////////////////////////////////////////////////
-TEST_F(DemTest, MissingFile)
+TEST_F(DemTest, MisingFile)
 {
   common::Dem dem;
   EXPECT_NE(dem.Load("/file/shouldn/never/exist.png"), 0);
@@ -38,7 +39,9 @@ TEST_F(DemTest, MissingFile)
 TEST_F(DemTest, NotDem)
 {
   common::Dem dem;
-  const auto path = common::testing::TestFile("CMakeLists.txt");
+  std::string path;
+
+  path = "file://media/materials/scripts/CMakeLists.txt";
   EXPECT_NE(dem.Load(path), 0);
 }
 
@@ -46,7 +49,9 @@ TEST_F(DemTest, NotDem)
 TEST_F(DemTest, UnsupportedDem)
 {
   common::Dem dem;
-  const auto path = common::testing::TestFile("data", "heightmap_bowl.png");
+  std::string path;
+
+  path = "file://media/materials/textures/wood.jpg";
   EXPECT_NE(dem.Load(path), 0);
 }
 
@@ -81,34 +86,30 @@ TEST_F(DemTest, BasicAPI)
   const auto path = common::testing::TestFile("data", "dem_squared.tif");
   EXPECT_EQ(dem.Load(path), 0);
 
-  // Check filename
-  EXPECT_EQ(path, dem.Filename());
-
   // Check the heights and widths
-  EXPECT_EQ(129, static_cast<int>(dem.Height()));
-  EXPECT_EQ(129, static_cast<int>(dem.Width()));
-  EXPECT_FLOAT_EQ(3984.4849, dem.WorldHeight());
-  EXPECT_FLOAT_EQ(3139.7456, dem.WorldWidth());
-  EXPECT_FLOAT_EQ(65.3583, dem.MinElevation());
-  EXPECT_FLOAT_EQ(318.441, dem.MaxElevation());
+  EXPECT_EQ(129, static_cast<int>(dem.GetHeight()));
+  EXPECT_EQ(129, static_cast<int>(dem.GetWidth()));
+  EXPECT_FLOAT_EQ(3984.4849, dem.GetWorldHeight());
+  EXPECT_FLOAT_EQ(3139.7456, dem.GetWorldWidth());
+  EXPECT_FLOAT_EQ(65.3583, dem.GetMinElevation());
+  EXPECT_FLOAT_EQ(318.441, dem.GetMaxElevation());
 
-  // Check Elevation()
-  unsigned int width = dem.Width();
-  unsigned int height = dem.Height();
-  EXPECT_FLOAT_EQ(215.82324, dem.Elevation(0, 0));
-  EXPECT_FLOAT_EQ(216.04961, dem.Elevation(width - 1, 0));
-  EXPECT_FLOAT_EQ(142.2274, dem.Elevation(0, height - 1));
-  EXPECT_FLOAT_EQ(209.14784, dem.Elevation(width - 1, height - 1));
+  // Check GetElevation()
+  unsigned int width = dem.GetWidth();
+  unsigned int height = dem.GetHeight();
+  EXPECT_FLOAT_EQ(215.82324, dem.GetElevation(0, 0));
+  EXPECT_FLOAT_EQ(216.04961, dem.GetElevation(width - 1, 0));
+  EXPECT_FLOAT_EQ(142.2274, dem.GetElevation(0, height - 1));
+  EXPECT_FLOAT_EQ(209.14784, dem.GetElevation(width - 1, height - 1));
 
   // Illegal coordinates
-  double inf = std::numeric_limits<double>::infinity();
-  EXPECT_DOUBLE_EQ(inf, dem.Elevation(0, height));
-  EXPECT_DOUBLE_EQ(inf, dem.Elevation(width, 0));
-  EXPECT_DOUBLE_EQ(inf, dem.Elevation(width, height));
+  ASSERT_ANY_THROW(dem.GetElevation(0, height));
+  ASSERT_ANY_THROW(dem.GetElevation(width, 0));
+  ASSERT_ANY_THROW(dem.GetElevation(width, height));
 
-  // Check GeoReferenceOrigin()
+  // Check GetGeoReferenceOrigin()
   ignition::math::Angle latitude, longitude;
-  EXPECT_TRUE(dem.GeoReferenceOrigin(latitude, longitude));
+  dem.GetGeoReferenceOrigin(latitude, longitude);
   EXPECT_FLOAT_EQ(38.001667, latitude.Degree());
   EXPECT_FLOAT_EQ(-122.22278, longitude.Degree());
 }
@@ -129,17 +130,17 @@ TEST_F(DemTest, FillHeightmap)
   std::vector<float> elevations;
 
   subsampling = 2;
-  vertSize = (dem.Width() * subsampling) - 1;
-  size.X(dem.WorldWidth());
-  size.Y(dem.WorldHeight());
-  size.Z(dem.MaxElevation() - dem.MinElevation());
+  vertSize = (dem.GetWidth() * subsampling) - 1;
+  size.X(dem.GetWorldWidth());
+  size.Y(dem.GetWorldHeight());
+  size.Z(dem.GetMaxElevation() - dem.GetMinElevation());
   scale.X(size.X() / vertSize);
   scale.Y(size.Y() / vertSize);
 
-  if (ignition::math::equal(dem.MaxElevation(), 0.0f))
+  if (ignition::math::equal(dem.GetMaxElevation(), 0.0f))
     scale.Z(fabs(size.Z()));
   else
-    scale.Z(fabs(size.Z()) / dem.MaxElevation());
+    scale.Z(fabs(size.Z()) / dem.GetMaxElevation());
   flipY = false;
 
   dem.FillHeightMap(subsampling, vertSize, size, scale, flipY, elevations);
@@ -148,46 +149,11 @@ TEST_F(DemTest, FillHeightmap)
   EXPECT_EQ(vertSize * vertSize, elevations.size());
 
   // Check the elevation of some control points
-  EXPECT_FLOAT_EQ(184.94113, elevations.at(0));
-  EXPECT_FLOAT_EQ(179.63583, elevations.at(elevations.size() - 1));
-  EXPECT_FLOAT_EQ(213.42966, elevations.at(elevations.size() / 2));
+  EXPECT_FLOAT_EQ(119.58285, elevations.at(0));
+  EXPECT_FLOAT_EQ(114.27753, elevations.at(elevations.size() - 1));
+  EXPECT_FLOAT_EQ(148.07137, elevations.at(elevations.size() / 2));
 }
-
-/////////////////////////////////////////////////
-TEST_F(DemTest, UnfinishedDem)
-{
-  common::Dem dem;
-  auto path = common::testing::TestFile("data", "dem_unfinished.tif");
-  EXPECT_EQ(dem.Load(path), 0);
-
-  // Check that the min and max elevations are valid for an unfinished
-  // and unfilled dem.
-  EXPECT_EQ(33, static_cast<int>(dem.Height()));
-  EXPECT_EQ(33, static_cast<int>(dem.Width()));
-  EXPECT_FLOAT_EQ(111287.59, dem.WorldHeight());
-  EXPECT_FLOAT_EQ(88878.297, dem.WorldWidth());
-  // gdal reports min elevation as -32768 but this is treated as a nodata
-  // by our dem class and ignored when computing the min elevation
-  EXPECT_FLOAT_EQ(-10, dem.MinElevation());
-  EXPECT_FLOAT_EQ(1909, dem.MaxElevation());
-
-  // test another dem file with multiple nodata values
-  common::Dem demNoData;
-
-  path = common::testing::TestFile("data", "dem_nodata.dem");
-  EXPECT_EQ(demNoData.Load(path), 0);
-
-  // Check that the min and max elevations are valid for a dem with multiple
-  // nodata values
-  EXPECT_EQ(65, static_cast<int>(demNoData.Height()));
-  EXPECT_EQ(65, static_cast<int>(demNoData.Width()));
-  EXPECT_FLOAT_EQ(7499.8281, demNoData.WorldHeight());
-  EXPECT_FLOAT_EQ(14150.225, demNoData.WorldWidth());
-  // gdal reports min elevation as -32767 but this is treated as a nodata
-  // by our dem class and ignored when computing the min elevation
-  EXPECT_FLOAT_EQ(682, demNoData.MinElevation());
-  EXPECT_FLOAT_EQ(2932, demNoData.MaxElevation());
-}
+#endif
 
 /////////////////////////////////////////////////
 int main(int argc, char **argv)
