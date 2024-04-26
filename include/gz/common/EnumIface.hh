c95da7c964883b2d5454b496d6076aa394404927
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-04-26 08:41:37 -0500

    Include SuppressWarning from utils
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- include/gz/common/EnumIface.hh
+++ include/gz/common/EnumIface.hh
@@ -24,6 +24,7 @@
 #include <gz/common/config.hh>
 #include <gz/common/Export.hh>
 #include <gz/common/Util.hh>
+#include "gz/utils/SuppressWarning.hh"
 
 namespace ignition
 {
@@ -142,7 +143,7 @@ namespace ignition
     ///   std::cout << "Type++ Name[" << myTypeIface.Str(*i) << "]\n";
     /// }
     /// \verbatim
-    GZ_UTILS_WARN_IGNORE__DEPRECATED_DECLARATION
+    IGN_UTILS_WARN_IGNORE__DEPRECATED_DECLARATION
     template<typename Enum>
     class EnumIterator
     : std::iterator<std::bidirectional_iterator_tag, Enum>
@@ -220,7 +221,7 @@ namespace ignition
       /// member value ever used.
       private: Enum c;
     };
-    GZ_UTILS_WARN_RESUME__DEPRECATED_DECLARATION
+    IGN_UTILS_WARN_RESUME__DEPRECATED_DECLARATION
 
     /// \brief Equality operator
     /// \param[in] _e1 First iterator
