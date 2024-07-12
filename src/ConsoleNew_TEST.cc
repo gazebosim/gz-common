9a318b6f89abd00a15302b712d59c2f35b72093e
Carlos Agüero <caguero@openrobotics.org>
2024-07-12 18:19:47 +0200

    Tests
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- src/ConsoleNew_TEST.cc
+++ src/ConsoleNew_TEST.cc
@@ -44,34 +44,34 @@ class ConsoleNew_TEST : public ::testing::Test
     EXPECT_TRUE(gz::common::unsetenv(GZ_HOMEDIR));
   }
 
-  /// \brief Read the log content.
-  /// \param[in] _filename Log filename.
-  /// \return Log content.
-  public: std::string GetLogContent(const std::string &_filename)
-  {
-    // Get the absolute path
-    std::string path;
-    EXPECT_TRUE(gz::common::env(GZ_HOMEDIR, path));
-    path = gz::common::joinPaths(path, _filename);
-    EXPECT_TRUE(gz::common::exists(path));
-
-    // Open the log file, and read back the string
-    std::ifstream ifs(path.c_str(), std::ios::in);
-    std::string loggedString;
-
-    while (!ifs.eof())
-    {
-      std::string line;
-      std::getline(ifs, line);
-      loggedString += line;
-    }
-
-    return loggedString;
-  }
-
   /// \brief Temporary directory to run test in.
   private: std::unique_ptr<gz::common::TempDirectory> temp;
 };
+
+/// \brief Read the log content.
+/// \param[in] _filename Log filename.
+/// \return Log content.
+std::string GetLogContent(const std::string &_filename)
+{
+  // Get the absolute path
+  std::string path;
+  EXPECT_TRUE(gz::common::env(GZ_HOMEDIR, path));
+  path = gz::common::joinPaths(path, _filename);
+  EXPECT_TRUE(gz::common::exists(path));
+
+  // Open the log file, and read back the string
+  std::ifstream ifs(path.c_str(), std::ios::in);
+  std::string loggedString;
+
+  while (!ifs.eof())
+  {
+    std::string line;
+    std::getline(ifs, line);
+    loggedString += line;
+  }
+
+  return loggedString;
+}
 
 /////////////////////////////////////////////////
 TEST_F(ConsoleNew_TEST, NonRootLogger)
@@ -85,6 +85,9 @@ TEST_F(ConsoleNew_TEST, NonRootLogger)
   gz::common::ConsoleNew testConsole("test");
   auto testLogger = testConsole.Logger();
 
+  testing::internal::CaptureStdout();
+  testing::internal::CaptureStderr();
+
   testLogger.log(spdlog::level::trace,    "This is a trace message");
   testLogger.log(spdlog::level::debug ,   "This is a debug message");
   testLogger.log(spdlog::level::info,     "This is an info message");
@@ -92,7 +95,14 @@ TEST_F(ConsoleNew_TEST, NonRootLogger)
   testLogger.log(spdlog::level::err,      "This is an error message");
   testLogger.log(spdlog::level::critical, "This is a critical error message");
 
-  getchar();
+  std::string stdOut = testing::internal::GetCapturedStdout();
+  std::string stdErr = testing::internal::GetCapturedStderr();
+
+  for (auto word : {"info"})
+    EXPECT_TRUE(stdOut.find(word) != std::string::npos);
+
+  for (auto word : {"warn", "error", "critical"})
+    EXPECT_TRUE(stdErr.find(word) != std::string::npos);
 }
 
 /////////////////////////////////////////////////
@@ -100,12 +110,24 @@ TEST_F(ConsoleNew_TEST, RootLogger)
 {
   auto gzLogger = gz::common::ConsoleNew::Root().Logger();
 
+  testing::internal::CaptureStdout();
+  testing::internal::CaptureStderr();
+
   gzLogger.log(spdlog::level::trace,    "This is a trace message");
   gzLogger.log(spdlog::level::debug ,   "This is a debug message");
   gzLogger.log(spdlog::level::info,     "This is an info message");
   gzLogger.log(spdlog::level::warn,     "This is a warn message");
   gzLogger.log(spdlog::level::err,      "This is an error message");
   gzLogger.log(spdlog::level::critical, "This is a critical error message");
+
+  std::string stdOut = testing::internal::GetCapturedStdout();
+  std::string stdErr = testing::internal::GetCapturedStderr();
+
+  for (auto word : {"info"})
+    EXPECT_TRUE(stdOut.find(word) != std::string::npos);
+
+  for (auto word : {"warn", "error", "critical"})
+    EXPECT_TRUE(stdErr.find(word) != std::string::npos);
 }
 
 /////////////////////////////////////////////////
@@ -115,12 +137,24 @@ TEST_F(ConsoleNew_TEST, RootLoggerColor)
   auto logger = spdlog::get("gz");
   ASSERT_TRUE(logger);
 
+  testing::internal::CaptureStdout();
+  testing::internal::CaptureStderr();
+
   logger->log(spdlog::level::trace,    "This is a trace message");
   logger->log(spdlog::level::debug ,   "This is a debug message");
   logger->log(spdlog::level::info,     "This is an info message");
   logger->log(spdlog::level::warn,     "This is a warn message");
   logger->log(spdlog::level::err,      "This is an error message");
   logger->log(spdlog::level::critical, "This is a critical error message");
+
+  std::string stdOut = testing::internal::GetCapturedStdout();
+  std::string stdErr = testing::internal::GetCapturedStderr();
+
+  for (auto word : {"info"})
+    EXPECT_TRUE(stdOut.find(word) != std::string::npos);
+
+  for (auto word : {"warn", "error", "critical"})
+    EXPECT_TRUE(stdErr.find(word) != std::string::npos);
 }
 
 /////////////////////////////////////////////////
@@ -130,23 +164,47 @@ TEST_F(ConsoleNew_TEST, RootLoggerNoColor)
   auto logger = spdlog::get("gz");
   ASSERT_TRUE(logger);
 
+  testing::internal::CaptureStdout();
+  testing::internal::CaptureStderr();
+
   logger->trace("This is a trace message");
   logger->debug("This is a debug message");
   logger->info("This is an info message");
   logger->warn("This is a warning message");
   logger->error("This is an error message");
   logger->critical("This is a critical message");
+
+  std::string stdOut = testing::internal::GetCapturedStdout();
+  std::string stdErr = testing::internal::GetCapturedStderr();
+
+  for (auto word : {"info"})
+    EXPECT_TRUE(stdOut.find(word) != std::string::npos);
+
+  for (auto word : {"warn", "error", "critical"})
+    EXPECT_TRUE(stdErr.find(word) != std::string::npos);
 }
 
 /////////////////////////////////////////////////
 TEST_F(ConsoleNew_TEST, RootLoggerMacros)
 {
+  testing::internal::CaptureStdout();
+  testing::internal::CaptureStderr();
+
   gztrace << "This is a trace message";
   gzdbg   << "This is a debug message";
   gzmsg   << "This is an info message";
   gzwarn  << "This is a warning message";
   gzerr   << "This is an error message";
   gzcrit  << "This is a critical message";
+
+  std::string stdOut = testing::internal::GetCapturedStdout();
+  std::string stdErr = testing::internal::GetCapturedStderr();
+
+  for (auto word : {"info"})
+    EXPECT_TRUE(stdOut.find(word) != std::string::npos);
+
+  for (auto word : {"warn", "error", "critical"})
+    EXPECT_TRUE(stdErr.find(word) != std::string::npos);
 }
 
 /////////////////////////////////////////////////
@@ -168,4 +226,8 @@ TEST_F(ConsoleNew_TEST, LogToFile)
   spdlog::warn("This is a warning message");
   spdlog::error("This is an error message");
   spdlog::critical("This is a critical message");
+
+  std::string logContent = GetLogContent("test.log");
+  for (auto word : {"info", "warn", "error", "critical"})
+    EXPECT_TRUE(logContent.find(word) != std::string::npos);
 }
